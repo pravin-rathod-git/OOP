8fd026ba3c9bb95ec39ca3d1deaecf370525a0da
pravin-rathod-git <pr819270@gmail.com>
2026-02-04 10:46:59 +0530

    creating the deep copy using copy constructor

---
+++ DeepCopyUsingCopyConstructor.java
@@ -0,0 +1,41 @@
+class Address {
+    String city;
+
+    Address(String city) {
+        this.city = city;
+    }
+
+    // Copy constructor
+    Address(Address a) {
+        this.city = a.city;
+    }
+}
+
+class Person {
+    String name;
+    Address address;
+
+    Person(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Copy constructor (Deep Copy)
+    Person(Person p) {
+        this.name = p.name;
+        this.address = new Address(p.address); // new object
+    }
+}
+
+public class DeepCopyUsingCopyConstructor {
+    public static void main(String[] args) {
+        Address addr = new Address("Mumbai");
+        Person p1 = new Person("Ravin", addr);
+        Person p2 = new Person(p1); // deep copy
+
+        p2.address.city = "Pune";
+
+        System.out.println(p1.address.city); // Mumbai
+        System.out.println(p2.address.city); // Pune
+    }
+}
